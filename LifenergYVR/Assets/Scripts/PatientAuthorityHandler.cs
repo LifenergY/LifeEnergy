7c0110b89762831345525d5269c07fef7f332ea1
Goel <g.vazquez.d@gmail.com>
2023-07-10 21:34:14 +0200

    Update PatientAuthorityHandler.cs

--- LifenergYVR/Assets/Scripts/PatientAuthorityHandler.cs
+++ LifenergYVR/Assets/Scripts/PatientAuthorityHandler.cs
@@ -4,36 +4,25 @@ using System.Collections.Generic;
 
 public class PatientAuthorityHandler : MonoBehaviour
 {
-    //[SerializeField] private List<NetworkObject> networkObjects;
+   // [SerializeField] private List<NetworkObject> networkObjects;
 
-    //private void Awake() => ExperienceModeManager.OnExperienceModeSelected += ModeSelected;
+    private void Awake() => ExperienceModeManager.OnExperienceModeSelected += ModeSelected;
 
-    //private void OnDestroy() => ExperienceModeManager.OnExperienceModeSelected -= ModeSelected;
+    private void OnDestroy() => ExperienceModeManager.OnExperienceModeSelected -= ModeSelected;
 
-    //private void ModeSelected(ExperienceMode mode)
-    //{
-    //    if (mode == ExperienceMode.Patient) RequestAuthority();
-    //}
+    private void ModeSelected(ExperienceMode mode)
+    {
+        if (mode == ExperienceMode.Patient) RequestAuthority();
+    }
 
-    //private void RequestAuthority()
-    //{
-    //    foreach (var networkObject in networkObjects)
-    //    {
-    //        networkObject.RequestStateAuthority();
-    //        RequestAuthorityInChildren(networkObject.gameObject);
-    //    }
-    //}
-
-    //private void RequestAuthorityInChildren(GameObject gameObject)
-    //{
-    //    foreach (Transform child in gameObject.transform)
-    //    {
-    //        var networkObject = child.GetComponent<NetworkObject>();
-    //        if (networkObject != null)
-    //        {
-    //            networkObject.RequestStateAuthority();
-    //        }
-    //        RequestAuthorityInChildren(child.gameObject);
-    //    }
-    //}
+    private void RequestAuthority()
+    {
+        // Find all objects with a NetworkObject component
+        NetworkObject[] allNetworkObjects = GameObject.FindObjectsOfType<NetworkObject>();
+        foreach (var networkObject in allNetworkObjects)
+        {
+            // Request authority
+            networkObject.RequestStateAuthority();
+        }
+    }
 }
